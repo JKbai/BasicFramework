1e16f91c06b3e9819a627e82e083e66a132e5d52
MyGitHub <bai_yinxiao@163.com>
2016-09-09 17:03:49 +0800

    项目目录框架规范

---
+++ BasicFramework/Expand/Const/Constants.h
@@ -0,0 +1,23 @@
+//
+//  Constants.h
+//  BasicFramework
+//
+//  Created by saifing on 16/9/9.
+//  Copyright © 2016年 baiyinxiao. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+/**
+ *  1.邮件接收人
+ */
+
+//static NSString* const kMail_ToRecipients_Address = @"bai_yinxiao@163.com";
+
+/**
+ *  客服电话
+ */
+//static NSString* const kCustomer_Service_iPhone = @"18710627862";
+
+#endif /* Constants_h */
---
+++ BasicFramework/Expand/Macros/CommonAppKey.h
@@ -0,0 +1,17 @@
+//
+//  CommonAppKey.h
+//  BasicFramework
+//
+//  Created by saifing on 16/9/9.
+//  Copyright © 2016年 baiyinxiao. All rights reserved.
+//
+
+#ifndef CommonAppKey_h
+#define CommonAppKey_h
+
+
+
+
+
+
+#endif /* CommonAppKey_h */
